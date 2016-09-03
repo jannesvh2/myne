2d20dd614aff3daa0799f742c36397513dbcc535
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-03 22:03:59 +0200

    removed tower code

--- jannes/role.tower.js
+++ jannes/role.tower.js
@@ -4,22 +4,22 @@ var roleTower = {
     run: function () {
 
         //tower
-        var tower = Game.getObjectById('579fa9450700be0674d2f083');
-        if (tower) {
-            //tower repair
-            var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-                filter: (structure) => structure.hits < structure.hitsMax
-            });
-            if (closestDamagedStructure) {
-                tower.repair(closestDamagedStructure);
-            }
+        //var tower = Game.getObjectById('579fa9450700be0674d2f083');
+        //if (tower) {
+        //    //tower repair
+        //    var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+        //        filter: (structure) => structure.hits < structure.hitsMax
+        //    });
+        //    if (closestDamagedStructure) {
+        //        tower.repair(closestDamagedStructure);
+        //    }
 
-            //tower attack
-            var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-            if (closestHostile) {
-                tower.attack(closestHostile);
-            }
-        }
+        //    //tower attack
+        //    var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        //    if (closestHostile) {
+        //        tower.attack(closestHostile);
+        //    }
+        //}
     }
 };
 
