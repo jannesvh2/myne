9d7358cbc102039c38dbf454fc0762e788ab7d5c
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-03 19:03:08 +0200

    fixed creep spawn

--- js/role.spawn.js
+++ js/role.spawn.js
@@ -9,28 +9,28 @@ var roleSpawn = {
             console.log('Clearing non-existing creep memory:', name);
         }
     }
-    var h = 1;
-    var b = 3;
+    var h = 2;
+    var b = 2;
     var u = 4;
 //spawn harvesters
 //get all harvesters
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     //console.log('Harvesters: ' + harvesters.length);
 //spawn if less than 2
-    if(harvesters.length < 2) {
+    if(harvesters.length < h) {
     var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE,MOVE], undefined, {role: 'harvester'});
     
     }
     
     var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
     //console.log('Builders: ' + builders.length);
-    if(builders.length < 2) {
+    if(builders.length < b) {
         var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE,MOVE], undefined, {role: 'builder'});
     }
     
         var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
     //console.log('Upgraders: ' + upgraders.length);
-    if(upgraders.length < 4) {
+    if(upgraders.length < u) {
         var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE,MOVE], undefined, {role: 'upgrader'});
     }
     console.log("h: " + harvesters.length + "/" + h + ", b: " + builders.length + "/" + b + ", u: " + upgraders.length + "/" + u);
