90cb365ecd63b777f02f5b6f8d597a4d94028783
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-03 23:23:34 +0200

    stop building if not enougth harvesters

--- jannes/role.builder.js
+++ jannes/role.builder.js
@@ -16,10 +16,10 @@ var roleBuilder = {
                 creep.say('repairing');
 
         }
-
+        var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
         if (creep.memory.building) {
             var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if (targets.length) {
+            if (targets.length && harvesters.length == 4) {
 
                 if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
