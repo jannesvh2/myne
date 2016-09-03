c629d3387964c3bb1b54489eba39a6b336107464
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-03 18:33:06 +0200

    changed builer/harvester amount

--- js/role.spawn.js
+++ js/role.spawn.js
@@ -14,14 +14,14 @@ var roleSpawn = {
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     //console.log('Harvesters: ' + harvesters.length);
 //spawn if less than 2
-    if(harvesters.length < 2) {
+    if(harvesters.length < 1) {
     var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE,MOVE], undefined, {role: 'harvester'});
         console.log('Spawning new harvester: ' + newName);
     }
     
     var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
     //console.log('Builders: ' + builders.length);
-    if(builders.length < 2) {
+    if(builders.length < 3) {
         var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE,MOVE], undefined, {role: 'builder'});
         console.log('Spawning new builder: ' + newName);
     }
