3a8d7eb5f8f054a747bc7375f64ae824d6e9d5ed
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-03 22:39:26 +0200

    added memory source to new spawns

--- jannes/role.spawn.js
+++ jannes/role.spawn.js
@@ -19,19 +19,21 @@ var roleSpawn = {
         //spawn if less than 2
         if (harvesters.length < h) {
             var newName = Game.spawns['Spawn1'].createCreep([WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE], undefined, { role: 'harvester' });
-
+            Game.creeps[newName].memory.source = 0;
         }
 
         var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
         //console.log('Builders: ' + builders.length);
         if (builders.length < b) {
             var newName = Game.spawns['Spawn1'].createCreep([WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE], undefined, { role: 'builder' });
+            Game.creeps[newName].memory.source = 0;
         }
 
         var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
         //console.log('Upgraders: ' + upgraders.length);
         if (upgraders.length < u) {
             var newName = Game.spawns['Spawn1'].createCreep([WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE], undefined, { role: 'upgrader' });
+            Game.creeps[newName].memory.source = 1;
         }
         console.log("h: " + harvesters.length + "/" + h + ", b: " + builders.length + "/" + b + ", u: " + upgraders.length + "/" + u);
     }
