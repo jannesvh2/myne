37db78e3092be2fb45ad0631f6c219d56913510d
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-03 23:18:02 +0200

    added more moving parts

--- jannes/role.spawn.js
+++ jannes/role.spawn.js
@@ -18,19 +18,19 @@ var roleSpawn = {
         //console.log('Harvesters: ' + harvesters.length);
         //spawn if less than 2
         if (harvesters.length < h) {
-            var newName = Game.spawns['Spawn1'].createCreep([WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE], undefined, { role: 'harvester',source: 0 });
+            var newName = Game.spawns['Spawn1'].createCreep([WORK, WORK, WORK, CARRY, MOVE, MOVE, MOVE], undefined, { role: 'harvester', source: 0 });
         }
 
         var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
         //console.log('Builders: ' + builders.length);
         if (builders.length < b) {
-            var newName = Game.spawns['Spawn1'].createCreep([WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE], undefined, { role: 'builder', source: 0 });
+            var newName = Game.spawns['Spawn1'].createCreep([WORK, WORK, WORK, CARRY, MOVE, MOVE, MOVE], undefined, { role: 'builder', source: 0 });
         }
 
         var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
         //console.log('Upgraders: ' + upgraders.length);
         if (upgraders.length < u) {
-            var newName = Game.spawns['Spawn1'].createCreep([WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE], undefined, { role: 'upgrader', source: 1 });
+            var newName = Game.spawns['Spawn1'].createCreep([WORK, WORK, WORK, CARRY, MOVE, MOVE, MOVE], undefined, { role: 'upgrader', source: 1 });
         }
         console.log("h: " + harvesters.length + "/" + h + ", b: " + builders.length + "/" + b + ", u: " + upgraders.length + "/" + u);
     }
