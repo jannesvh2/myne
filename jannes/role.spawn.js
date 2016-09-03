73c8e5975290f3ea40afeb5373378c81dbac25c0
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-03 21:31:57 +0200

    added more builders

--- jannes/role.spawn.js
+++ jannes/role.spawn.js
@@ -9,9 +9,9 @@ var roleSpawn = {
                 console.log('Clearing non-existing creep memory:', name);
             }
         }
-        var h = 2;
-        var b = 2;
-        var u = 4;
+        var h = 3;
+        var b = 5;
+        var u = 2;
         //spawn harvesters
         //get all harvesters
         var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
