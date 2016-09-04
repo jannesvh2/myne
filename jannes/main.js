dcbff29ef7d19df9b40ae6ac1ea90d52bd0256e9
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-04 14:57:11 +0200

    hard rester

--- jannes/main.js
+++ jannes/main.js
@@ -9,8 +9,8 @@ var rolePath = require('role.path2');
 module.exports.loop = function () {
     PathFinder.use(true);
     var h = 2;
-    var b = 3;
-    var u = 6;
+    var b = 6;
+    var u = 3;
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
     var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
