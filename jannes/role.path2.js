80fac1edcf79855064e0f70b78013604a4974e50
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-04 13:48:25 +0200

    fix

--- jannes/role.path2.js
+++ jannes/role.path2.js
@@ -5,6 +5,8 @@ var rolePath = {
 
         var path = spawn.pos.findPathTo(RoomPosition(4, 7, 'W14N58'));
         Memory.path = Room.serializePath(path);
+        creep.say("path");
+        console.log(Memory.path);
         creep.moveByPath(Memory.path);
         creep.build(STRUCTURE_ROAD);
     }
