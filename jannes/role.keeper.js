df69f272cf387fac15525bc1c4d93f45df83774b
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-04 16:52:28 +0200

    fix

--- jannes/role.keeper.js
+++ jannes/role.keeper.js
@@ -3,11 +3,8 @@ var roleKeeper = {
     /** @param {Creep} creep **/
     run: function (creep) {
         //clearing memory of non existing creeps
-        var path = creep.pos.findPathTo(21, 18, 'W14N58');
-        Memory.path = Room.serializePath(path);
-        creep.say("path");
-        console.log(Memory.path);
-        creep.moveByPath(Memory.path);
+        creep.say("keeper");
+        creep.moveTo(21, 18, 'W14N58');
     }
 };
 
