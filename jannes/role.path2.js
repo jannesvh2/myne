feb3eb886dd25823da736479d6a805d8c39c364a
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-04 14:20:15 +0200

    fixed path

--- jannes/role.path2.js
+++ jannes/role.path2.js
@@ -2,24 +2,15 @@ var rolePath = {
 
     /** @param {Creep} creep **/
     run: function (creep) {
-        if (creep.memory.path == null) {
             var path = creep.pos.findPathTo(4, 7, 'W14N58');
             Memory.path = Room.serializePath(path);
             creep.say("path");
             console.log(Memory.path);
             creep.moveByPath(Memory.path);
-            if (creep.pos.room == 'W14N59 ')
+            if (creep.pos.roomName == 'W14N59 ')
                 Game.rooms.W14N59.createConstructionSite(10, 15, STRUCTURE_ROAD);
-            if (creep.pos.room == 'W14N58 ')
+            if (creep.pos.roomName == 'W14N58 ')
                 Game.rooms.W14N58.createConstructionSite(10, 15, STRUCTURE_ROAD);
-        }
-        else {
-            creep.moveByPath(Memory.path);
-            if (creep.pos.room == 'W14N59 ')
-                Game.rooms.W14N59.createConstructionSite(10, 15, STRUCTURE_ROAD);
-            if (creep.pos.room == 'W14N58 ')
-                Game.rooms.W14N58.createConstructionSite(10, 15, STRUCTURE_ROAD);
-        }
     }
 };
 
