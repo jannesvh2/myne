5273f7fb87ccc84ab88b9f8f1db4ac51de872e81
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-04 13:58:35 +0200

    fixed path

--- jannes/role.path2.js
+++ jannes/role.path2.js
@@ -3,7 +3,7 @@ var rolePath = {
     /** @param {Creep} creep **/
     run: function (creep) {
 
-        var path = spawn.pos.findPathTo(RoomPosition(4, 7, 'W14N58'));
+        var path = creep.pos.findPathTo(4, 7, 'W14N58');
         Memory.path = Room.serializePath(path);
         creep.say("path");
         console.log(Memory.path);
