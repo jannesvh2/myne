69a983b471183aa3108737fe42be940cc60d9078
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-04 18:30:04 +0200

    fix

--- jannes/role.tower.js
+++ jannes/role.tower.js
@@ -4,23 +4,25 @@ var roleTower = {
     run: function () {
 
         //tower
-        var tower = Game.getObjectById('57cc3ed5c78dae3f28b4dfd0');
-        if (tower) {
-            //tower attack
-            var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-            if (closestHostile) {
-                tower.attack(closestHostile);
-            }
-            else {
-                //tower repair
-                var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-                    filter: (structure) => structure.hits < structure.hitsMax
-                });
-                if (closestDamagedStructure) {
-                    tower.repair(closestDamagedStructure);
+        try {
+            var tower = Game.getObjectById('57cc3ed5c78dae3f28b4dfd0');
+            if (tower) {
+                //tower attack
+                var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+                if (closestHostile) {
+                    tower.attack(closestHostile);
+                }
+                else {
+                    //tower repair
+                    var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+                        filter: (structure) => structure.hits < structure.hitsMax
+                    });
+                    if (closestDamagedStructure) {
+                        tower.repair(closestDamagedStructure);
+                    }
                 }
             }
-        }
+        } catch (err) { }
     }
 };
 
