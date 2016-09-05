829e345e90794137f2035c07c69b2c9f448828e0
jannesvh <jvanhelsdingen@retrorabbit.co.za>
2016-09-05 11:13:42 +0200

    fix

--- jannes/role.repairer.js
+++ jannes/role.repairer.js
@@ -1,26 +1,29 @@
 var roleRepairer = {
-    run: function(creep) {
+    run: function (creep) {
         var closestDamagedStructure = creep.pos.findClosestByRange(FIND_STRUCTURES, {
             filter: (structure) => {
                 return (structure.hits < structure.hitsMax &&
                     structure.hits < 100000)
-            }});
+            }
+        });
         if (!closestDamagedStructure) {
             for (var myRooms in Game.rooms) {
                 if (!closestDamagedStructure) {
                     closestDamagedStructure = Game.rooms[myRooms].find(FIND_STRUCTURES, {
                         filter: (structure) => {
-                             return (structure.hits < structure.hitsMax &&
-                                 structure.hits < 100000)
-                }
+                            return (structure.hits < structure.hitsMax &&
+                                structure.hits < 100000)
+                        }
                     });
                 }
             }
 
+            
+        }
         if (closestDamagedStructure) {
             creep.repair(closestDamagedStructure);
         }
-	}
-};
+    }
+}
 
 module.exports = roleRepairer;
