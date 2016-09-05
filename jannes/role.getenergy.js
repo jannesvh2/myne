45da8867a21bf826b8a188dc2c85ab40b733009d
jannesvh <jvanhelsdingen@retrorabbit.co.za>
2016-09-05 14:56:32 +0200

    fix

--- jannes/role.getenergy.js
+++ jannes/role.getenergy.js
@@ -2,11 +2,11 @@ var roleGetEnergy = {
     run: function (creep, sources) {
         //go to closest source
         var source;
-        if (!creep.memory.sourceId) {
+        if (!creep.memory.source) {
 
             for (var s in sources) {
-                creep.memory.sourceId = creep.pos.findClosestByPath(sources).id;
-                if (Memory.avgAtSource[creep.memory.sourceId] > 2)
+                creep.memory.source = creep.pos.findClosestByPath(sources);
+                if (Memory.avgAtSource[creep.memory.source.id] > 2)
                     for (var i = 0; i < sources.length; i++) {
                         if (sources[i] == s) {
                             sources.splice(i, 1);
@@ -19,11 +19,11 @@ var roleGetEnergy = {
                 }
             }
         }
-        if (creep.harvest(sources[creep.memory.sourceId]) == ERR_NOT_IN_RANGE) {
-            if (creep.moveTo(sources[creep.memory.sourceId]) == OK) { }
+        if (creep.harvest(sources[creep.memory.source]) == ERR_NOT_IN_RANGE) {
+            if (creep.moveTo(sources[creep.memory.source]) == OK) { }
         }
         else
-            Memory.atSources[creep.memory.sourceId]++;
+            Memory.atSources[creep.memory.source.id]++;
     }
 
 };
