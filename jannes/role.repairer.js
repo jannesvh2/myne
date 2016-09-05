fcd92e66dcfa2feea465ac7897197e64e8b75831
jannesvh <jvanhelsdingen@retrorabbit.co.za>
2016-09-05 10:35:33 +0200

    repair up to 100000

--- jannes/role.repairer.js
+++ jannes/role.repairer.js
@@ -1,20 +1,21 @@
 var roleRepairer = {
     run: function(creep) {
         var closestDamagedStructure = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => structure.hits < structure.hitsMax
-        });
+            filter: (structure) => {
+                return (structure.hits < structure.hitsMax &&
+                    structure.hits < 100000)
+            }});
         if (!closestDamagedStructure) {
-            closestDamagedStructure = Game.rooms.W14N59.find(FIND_STRUCTURES, {
-                filter: (structure) => structure.hits < structure.hitsMax
-            });
-        }
-        if (!closestDamagedStructure) {
-            try {
-                closestDamagedStructure = Game.rooms.W14N58.find(FIND_STRUCTURES, {
-                    filter: (structure) => structure.hits < structure.hitsMax
-                });
-            } catch (err) { }
-        }
+            for (var myRooms in Game.rooms) {
+                if (!closestDamagedStructure) {
+                    closestDamagedStructure = Game.rooms[myRooms].find(FIND_STRUCTURES, {
+                        filter: (structure) => {
+                             return (structure.hits < structure.hitsMax &&
+                                 structure.hits < 100000)
+                }
+                    });
+                }
+            }
 
         if (closestDamagedStructure) {
             creep.repair(closestDamagedStructure);
