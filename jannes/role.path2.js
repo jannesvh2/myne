64726ea5da009364d43166a7acc1a4e823323d54
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-05 23:10:24 +0200

    fix

--- jannes/role.path2.js
+++ jannes/role.path2.js
@@ -2,6 +2,7 @@ var rolePath = {
 
     /** @param {Creep} creep **/
     run: function (creep) {
+        //Game.spawns['Spawn1'].createCreep([MOVE], undefined, { role: 'path2' });
         var targetLocation = Game.getObjectById('579fa86e0700be0674d2d98c');
         var sourceRoom = 'W58S28';
         if (creep.room.name != sourceRoom && sourceRoom != '') {
