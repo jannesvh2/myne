5e85e0963d21406d1e08d1e386855bc879e8626c
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-05 22:29:13 +0200

    fix

--- jannes/role.path2.js
+++ jannes/role.path2.js
@@ -2,13 +2,18 @@ var rolePath = {
 
     /** @param {Creep} creep **/
     run: function (creep) {
-        var path = creep.pos.findPathTo(4, 7, 'W14N58');
-        Memory.path = Room.serializePath(path);
-        creep.moveByPath(Memory.path);
-        if (creep.pos.roomName == 'W14N59')
-            Game.rooms.W14N59.createConstructionSite(creep.pos.x, creep.pos.y, STRUCTURE_ROAD);
-        if (creep.pos.roomName == 'W14N58')
-            Game.rooms.W14N58.createConstructionSite(creep.pos.x, creep.pos.y, STRUCTURE_ROAD);
+        var targetLocation = Game.getObjectById('579fa86e0700be0674d2d98c');
+        var sourceRoom = 'W58S28';
+        if (creep.room.name != sourceRoom && sourceRoom != '') {
+            var exitDir = Game.map.findExit(creep.room.name, sourceRoom);
+            var Exit = creep.pos.findClosestByRange(exitDir);
+            creep.moveTo(Exit);
+            Game.rooms[creep.room.name].createConstructionSite(creep.pos.x, creep.pos.y, STRUCTURE_ROAD);
+        }
+        else if (creep.pos != targetLocation.pos) {
+            creep.moveTo(targetLocation)
+            Game.rooms[creep.room.name].createConstructionSite(creep.pos.x, creep.pos.y, STRUCTURE_ROAD);
+        }
     }
 };
 
