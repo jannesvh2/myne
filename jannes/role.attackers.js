8f5af6fb27173bc05893d2a6c794fe1dcdb649e3
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-05 19:22:21 +0200

    fix

--- jannes/role.attackers.js
+++ jannes/role.attackers.js
@@ -4,17 +4,24 @@ var roleAttackers = {
     run: function () {
         var enableID = false;
         var enablePriority = false;
+        var targetLocation = Game.getObjectById('57cc37bffca092544afcc82d');
+        var priorityTarget = Game.getObjectById('57cc37bffca092544afcc82d');
 
-        if (creep.pos != Game.getObjectById('57cc37bffca092544afcc82d').pos) {
-            //clearing memory of non existing creeps
-            var path = creep.pos.findPathTo(Game.getObjectById('57cc37bffca092544afcc82d'));
-            Memory.path = Room.serializePath(path);
-            creep.moveByPath(Memory.path);
+        var sourceRoom = targetLocation.room.name;
+
+        //If not in the correct room, move towards it
+        if (creep.room.name != sourceRoom && sourceRoom != '') {
+            var exitDir = Game.map.findExit(creep.room.name, sourceRoom);
+            var Exit = creep.pos.findClosestByPath(exitDir);
+            creep.moveTo(Exit);
+        }
+        else if (creep.pos != targetLocation.pos) {
+            creep.moveTo(targetLocation)
         }
         //Priority attack
         var targets = [];
         if (enableID) {
-            targets.push(Game.getObjectById('57cc37bffca092544afcc82d'));
+            targets.push(priorityTarget);
         }
 
         else if (enablePriority) {
