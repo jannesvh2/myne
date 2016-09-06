deabe065349242184dba9026f9d9e9c6415d6140
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-07 00:19:15 +0200

    fix

--- jannes/role.getenergy.js
+++ jannes/role.getenergy.js
@@ -7,14 +7,13 @@ var roleGetEnergy = {
                 console.log(creep.name);
                 var tmpsources = sources;
 
-                for (var s = 0; s < tmpsources.length; s++) {
-                    s = 0;
+                while (tmpsources.length) {
+                    var s = 0;
                     if (tmpsources.length == 0)
                         break;
                     creep.memory.sourceId = creep.pos.findClosestByRange(tmpsources).id;
-                    if (Memory.avgAtSource[creep.memory.sourceId] > 2.5 || tmpsources[s].energy < 400) {
+                    if (Memory.avgAtSource[creep.memory.sourceId] > 2.5 || Game.getObjectById(creep.memory.sourceId).energy < 300) {
                         tmpsources.splice(s, 1);
-                        s--;
                     }
                     else {
                         break;
@@ -24,10 +23,10 @@ var roleGetEnergy = {
         }
         newSource();
         var creepSource = Game.getObjectById(creep.memory.sourceId);
-        if (creepSource.ticksToRegeneration === undefined || (creepSource.ticksToRegeneration < 30 && creepSource.energy < 200 && creep.energy == 0)) {
+        if (creepSource.ticksToRegeneration === undefined || (creepSource.ticksToRegeneration < 30 && creepSource.energy < 300 && creep.energy == 0)) {
             delete creep.memory.sourceId;
             newSource();
-            console.log(creep.name);
+            console.log(creep.name +" "+ creepSource.ticksToRegeneration);
             creepSource = Game.getObjectById(creep.memory.sourceId);
         }
         var sourceEmpty = creep.harvest(creepSource);
