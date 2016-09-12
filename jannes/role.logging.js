023198d52f1a13a489f232738f933e18daead626
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-12 20:15:20 +0200

    fix

--- jannes/role.logging.js
+++ jannes/role.logging.js
@@ -14,7 +14,7 @@ var roleLogging = {
             + ", U: " + Memory.spawns[0].creeps.upgraders2.length + "/" + Memory.spawns[0].summon.u2
             + ", a: " + Memory.spawns[0].creeps.attackers.length + "/" + Memory.spawns[0].summon.atk
             + ", s: " + Memory.spawns[0].creeps.scouts.length + "/" + Memory.spawns[0].spots.length
-            + ", ST: " + Memory.spawns[0].creeps.stores.length + "/" + Memory.spawns[0].sources.sources.length;
+            + ", ST: " + Memory.spawns[0].creeps.stores.length + "/" + Memory.spawns[0].sources.length;
         //+ ", l: " + links.length + "/" + Memory.linkSource.length * 2;
 
         console.log(msg);
