642bc9dba30ce99ea36d3e5b142204990667e0c0
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-12 20:16:30 +0200

    fix

--- jannes/role.tower.js
+++ jannes/role.tower.js
@@ -28,7 +28,7 @@ var roleTower = {
         var hostiles = Game.rooms.W59S29.find(FIND_HOSTILE_CREEPS);
         var towers = Game.rooms.W59S29.find(
             FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_TOWER } });
-        var targetHeal = Game.rooms.W59S29.findClosestByRange(FIND_MY_CREEPS, {
+        var targetHeal = Game.rooms.W59S29.find(FIND_MY_CREEPS, {
             filter: function (object) {
                 return object.hits < object.hitsMax;
             }
