899c37883736aa5586ec0969f1a0700bc14fc38f
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-14 00:00:27 +0200

    fix

--- jannes/role.keeper.js
+++ jannes/role.keeper.js
@@ -5,21 +5,29 @@ var roleKeeper = {
 
         //If not in the correct room, move towards it
         for (let spot = 0, length = Memory.spawns[creep.memory.spawn].spots.length; spot < length; spot++) {
-            if (creep.memory.sourceRoom == Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom) {
-                if (creep.room.name != Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom && Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom != '') {
-                    var exitDir = Game.map.findExit(creep.room.name, Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom);
-                    var Exit = creep.pos.findClosestByRange(exitDir);
-                    creep.moveTo(Exit, { maxOps: 5000 });
+
+            var reserve = creep.reserveController(creep.room.controller);
+            if (reserve == ERR_NOT_IN_RANGE)
+                creep.moveTo(creep.room.controller, { maxOps: 5000 });
+            else {
+                if (creep.memory.sourceRoom == Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom) {
+                    if (creep.room.name != Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom && Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom != '') {
+                        var exitDir = Game.map.findExit(creep.room.name, Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom);
+                        var Exit = creep.pos.findClosestByRange(exitDir);
+                        creep.moveTo(Exit, { maxOps: 5000 });
+                    }
+                    else if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(creep.room.controller, { maxOps: 5000 });
+
+                    }
+                    break;
                 }
-                else if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(creep.room.controller, { maxOps: 5000 });
-                    
-                }
-                break;
             }
+
+
         }
-       
-    
+
+
     }
 };
 
