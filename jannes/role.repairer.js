83fea4d7ac4de8ef8f6189cdf0d0873444261c16
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-14 01:43:07 +0200

    fix

--- jannes/role.repairer.js
+++ jannes/role.repairer.js
@@ -6,7 +6,7 @@ var roleRepairer = {
                 }
             });
         if (!closestDamagedStructure) {
-            for (let myRooms, length = Memory.spawns[creep.memory.spawn].random.rooms.length; myRooms < length; myRooms++) {
+            for (let myRooms = 0, length = Memory.spawns[creep.memory.spawn].random.rooms.length; myRooms < length; myRooms++) {
                 if (!closestDamagedStructure) {
                     closestDamagedStructure = Game.rooms[Memory.spawns[creep.memory.spawn].random.rooms[myRooms]].find(FIND_STRUCTURES, {
                         filter: (structure) => {
