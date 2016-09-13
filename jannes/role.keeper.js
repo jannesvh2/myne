e474d310e04d7e7b3bf049ac81bca3e2b340d3f3
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-14 01:12:35 +0200

    fix

--- jannes/role.keeper.js
+++ jannes/role.keeper.js
@@ -2,25 +2,28 @@ var roleKeeper = {
 
     /** @param {Creep} creep **/
     run: function (creep) {
+        if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE)
+            creep.moveTo(new RoomPosition(creep.room.controller.x, creep.room.controller.y, anotherRoomName), { maxOps: 5000 });
+        else
+            //If not in the correct room, move towards it
+            for (let spot = 0, length = Memory.spawns[creep.memory.spawn].spots.length; spot < length; spot++) {
 
-        //If not in the correct room, move towards it
-        for (let spot = 0, length = Memory.spawns[creep.memory.spawn].spots.length; spot < length; spot++) {
 
-            if (creep.memory.sourceRoom == Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom) {
-                if (creep.room.name != Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom && Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom != '') {
-                    var exitDir = Game.map.findExit(creep.room.name, Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom);
-                    var Exit = creep.pos.findClosestByRange(exitDir);
-                    creep.moveTo(Exit, { maxOps: 5000 });
+                if (creep.memory.sourceRoom == Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom) {
+                    if (creep.room.name != Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom && Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom != '') {
+                        var exitDir = Game.map.findExit(creep.room.name, Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom);
+                        var Exit = creep.pos.findClosestByRange(exitDir);
+                        creep.moveTo(Exit, { maxOps: 5000 });
+                    }
+                    else if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(creep.room.controller, { maxOps: 5000 });
+
+                    }
+                    break;
                 }
-                else if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(creep.room.controller, { maxOps: 5000 });
-                    
-                }
-                break;
             }
-        }
-       
-    
+
+
     }
 };
 
