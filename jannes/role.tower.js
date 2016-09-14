61c44267773e7f1ae712b70805ff31f87fbd6148
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-14 18:11:11 +0200

    fix

--- jannes/role.tower.js
+++ jannes/role.tower.js
@@ -4,13 +4,13 @@ var roleTower = {
     run: function () {
 
         //tower
-        var twrRep = function (tower) {
+        var twrRep = function (tower, a) {
             if (tower.energy > tower.energyCapacity * 2 / 3) {
                 //tower repair
                 var closestDamagedStructure = tower.pos.findClosestByRange(FIND_MY_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.hits < structure.hitsMax - 750 &&
-                            structure.hits < Memory.spawns[0].counters.repairLimit - 30000)
+                            structure.hits < Memory.spawns[a].counters.repairLimit - 30000)
                     }
                 });
                 if (!closestDamagedStructure) {
@@ -25,26 +25,27 @@ var roleTower = {
                 }
             }
         }
-        var hostiles = Game.rooms[Memory.spawns[0].random.mainRoom].find(FIND_HOSTILE_CREEPS);
-        var towers = Game.rooms[Memory.spawns[0].random.mainRoom].find(
-            FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_TOWER } });
-        var targetHeal = Game.rooms[Memory.spawns[0].random.mainRoom].find(FIND_MY_CREEPS, {
-            filter: function (object) {
-                return object.hits < object.hitsMax;
+        for (let a = 0, length = Game.spawns.length; a < length; a++) {
+            var hostiles = Game.rooms[Memory.spawns[a].random.mainRoom].find(FIND_HOSTILE_CREEPS);
+            var towers = Game.rooms[Memory.spawns[a].random.mainRoom].find(
+                FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_TOWER } });
+            var targetHeal = Game.rooms[Memory.spawns[a].random.mainRoom].find(FIND_MY_CREEPS, {
+                filter: function (object) {
+                    return object.hits < object.hitsMax;
+                }
+            });
+            if (hostiles.length > 0) {
+                var username = hostiles[0].owner.username;
+                if (username != 'Invader')
+                    Game.notify(`User ${username} spotted in room ${Memory.spawns[a].random.mainRoom}`);
+                towers.forEach(tower => tower.attack(hostiles[0]));
             }
-        });
-        if (hostiles.length > 0) {
-            var username = hostiles[0].owner.username;
-            if (username != 'Invader')
-            Game.notify(`User ${username} spotted in room ${Memory.spawns[0].random.mainRoom}`);
-            towers.forEach(tower => tower.attack(hostiles[0]));
-        }
-        else if (targetHeal.length) {
+            else if (targetHeal.length) {
                 towers.forEach(tower => tower.heal(targetHeal[0]));
+            }
+            else
+                towers.forEach(tower => twrRep(tower, a));
         }
-        else
-            towers.forEach(tower => twrRep(tower));
-
 
     }
 };
