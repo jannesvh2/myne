ee8e2ed106ff88942b80fc0133f923c381c8ba99
jannesvh <jvanhelsdingen@retrorabbit.co.za>
2016-09-14 09:36:33 +0200

    fix

--- jannes/role.spawn.js
+++ jannes/role.spawn.js
@@ -51,7 +51,7 @@ var roleSpawn = {
             }
         }
 
-        else if (Memory.spawns[spawn].spots.length) {
+        if (Memory.spawns[spawn].spots.length) {
             if (didSpawn == false) {
                 for (let scout = 0, length = Memory.spawns[spawn].spots.length; scout < length; scout++) {
                     let ticks = _.filter(scouts, (creep) => creep.memory.sourceRoom == Memory.spawns[spawn].spots[scout].sourceRoom);
