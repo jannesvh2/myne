253b85dd395ae54dcc8c7a481adfac05a0c35b3c
jannesvh <jvanhelsdingen@retrorabbit.co.za>
2016-09-14 09:48:09 +0200

    fix

--- jannes/role.keeper.js
+++ jannes/role.keeper.js
@@ -4,11 +4,12 @@ var roleKeeper = {
     run: function (creep) {
         for (let spot = 0, length = Memory.spawns[creep.memory.spawn].spots.length; spot < length; spot++) {
             //If not in the correct room, move towards it
-            if (creep.memory.sourceRoom == Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom && creep.memory.sourceRoom == creep.room.name) {
-                let reserveCheck = creep.reserveController(creep.room.controller);
-                if (reserveCheck == ERR_NOT_IN_RANGE)
-                    creep.moveTo(new RoomPosition(Game.rooms[Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom].controller.pos.x, Game.rooms[Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom].controller.pos.y, Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom), { maxOps: 5000 });
-
+            if (creep.memory.sourceRoom == Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom) {
+                if (creep.memory.sourceRoom == creep.room.name) {
+                    let reserveCheck = creep.reserveController(creep.room.controller);
+                    if (reserveCheck == ERR_NOT_IN_RANGE)
+                        creep.moveTo(new RoomPosition(Game.rooms[Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom].controller.pos.x, Game.rooms[Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom].controller.pos.y, Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom), { maxOps: 5000 });
+                }
                 else if (reserveCheck != OK) {
                     if (creep.room.name != Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom && Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom != '') {
                         var exitDir = Game.map.findExit(creep.room.name, Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom);
