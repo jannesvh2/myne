0804b6bbe73a46cca95c84569ad0b935abdbf459
jannesvh <jvanhelsdingen@retrorabbit.co.za>
2016-09-15 11:08:18 +0200

    fix

--- jannes/role.defender.js
+++ jannes/role.defender.js
@@ -13,11 +13,19 @@ var roleDefenders = {
             creep.moveTo(Exit, { maxOps: 5000 });
         }
         else {
+            if (creep.pos.x == 0)
+                creep.move(RIGHT);
+            else if (creep.pos.y == 0)
+                creep.move(BOTTOM);
+            else if (creep.pos.x == 49)
+                creep.move(LEFT);
+            else if (creep.pos.y == 49)
+                creep.move(TOP);
             var targets = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
 
             if (targets) {
 
-                if (creep.attack(targets) == ERR_NOT_IN_RANGE) creep.moveTo(targets);
+                if (creep.attack(targets) == ERR_NOT_IN_RANGE) creep.moveTo(targets, { maxOps: 5000 });
             }
         }
     }
