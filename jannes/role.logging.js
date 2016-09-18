f62240b60fa6330979eeb2bdd69c2fcc16c930ba
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-18 23:55:35 +0200

    fix

--- jannes/role.logging.js
+++ jannes/role.logging.js
@@ -4,12 +4,12 @@ var roleLogging = {
     run: function () {
 
         var msg = '';
-        msg += '( W59S29 has ' + Game.rooms.W59S29.energyAvailable + ' energy | ';
+       // msg += '( W59S29 has ' + Game.rooms.W59S29.energyAvailable + ' energy | ';
         msg +=
             //"h: " + harvesters.length + "/" + h
             //+ ", b: " + builders.length + "/" + b
             //+ ", u: " + upgraders.length + "/" + u
-             ", H: " + Memory.spawns[0].creeps.harvesters2.length + "/" + Memory.spawns[0].summon.h2
+             "( W59S29 has , H: " + Memory.spawns[0].creeps.harvesters2.length + "/" + Memory.spawns[0].summon.h2
             + ", B: " + Memory.spawns[0].creeps.builders2.length + "/" + Memory.spawns[0].summon.b2
             + ", U: " + Memory.spawns[0].creeps.upgraders2.length + "/" + Memory.spawns[0].summon.u2
             + ", m/r: " + Memory.spawns[0].creeps.attackersM.length + "/" + Memory.spawns[0].summon.atkM +" "+ Memory.spawns[0].creeps.attackersR.length + "/" + Memory.spawns[0].summon.atkR
@@ -17,12 +17,12 @@ var roleLogging = {
             + ", ST: " + Memory.spawns[0].creeps.stores.length + "/" + parseInt(Memory.spawns[0].sources.length)
             + " | rep: " + String(Memory.spawns[0].counters.repairLimit).replace(/(.)(?=(\d{3})+$)/g, '$1,');
 
-        msg += ' ) ( W56S28 has ' + Game.rooms.W56S28.energyAvailable + ' energy | ';
+      //  msg += ' ) ( W56S28 has ' + Game.rooms.W56S28.energyAvailable + ' energy | ';
         msg +=
             //"h: " + harvesters.length + "/" + h
             //+ ", b: " + builders.length + "/" + b
             //+ ", u: " + upgraders.length + "/" + u
-             ", H: " + Memory.spawns[1].creeps.harvesters2.length + "/" + Memory.spawns[1].summon.h2
+             "( W56S28 has , H: " + Memory.spawns[1].creeps.harvesters2.length + "/" + Memory.spawns[1].summon.h2
             + ", B: " + Memory.spawns[1].creeps.builders2.length + "/" + Memory.spawns[1].summon.b2
             + ", U: " + Memory.spawns[1].creeps.upgraders2.length + "/" + Memory.spawns[1].summon.u2
             + ", m/r: " + Memory.spawns[1].creeps.attackersM.length + "/" + Memory.spawns[1].summon.atkM +" "+ Memory.spawns[1].creeps.attackersR.length + "/" + Memory.spawns[1].summon.atkR
