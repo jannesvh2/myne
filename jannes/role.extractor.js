cd331a4d100dcffc6b467187dc2eb0ba4acbf293
jannesvh <jvanhelsdingen@retrorabbit.co.za>
2016-09-19 12:33:25 +0200

    fix

--- jannes/role.extractor.js
+++ jannes/role.extractor.js
@@ -15,7 +15,8 @@ var roleExtractor = {
         if (creep.memory.full) {
 
             var terminal = Game.getObjectById(Memory.spawns[creep.memory.spawn].random.terminal.id);
-            if (terminal.store.energy < 240000) {
+            var total = _.sum(terminal.store);
+            if (total < 240000) {
                 for (var resource in creep.carry) {
                     if (creep.carry[resource] > 0) {
                         if (creep.transfer(terminal, resource) == ERR_NOT_IN_RANGE)
