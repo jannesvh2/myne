8014e26aa78f580b500adf86cd48ec67efe7e013
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-22 01:55:08 +0200

    fix

--- jannes/role.spawn.js
+++ jannes/role.spawn.js
@@ -62,7 +62,7 @@ var roleSpawn = {
 
             }
         }
-        if (Memory.spawns[spawn].random.useStore && Memory.spawns[spawn].creeps.users.length < 3) {
+        if (Memory.spawns[spawn].random.useStore && Memory.spawns[spawn].creeps.users.length < 2) {
             if (didSpawn == false) {
                 newName = Game.spawns['Spawn' + parseInt(spawn + 1)].createCreep([CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE], undefined, { role: 'user', spawn: spawn });
                 return;
