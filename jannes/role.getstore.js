39f92e03996faf0283c9202879578de3fc012f54
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-22 03:38:36 +0200

    fix

--- jannes/role.getstore.js
+++ jannes/role.getstore.js
@@ -1,27 +1,28 @@
 var roleGetStore = {
     run: function (creep) {
         //go to closest source
-       // if (creep.memory.role != 'builder' && creep.memory.role != 'builder2')
+        // if (creep.memory.role != 'builder' && creep.memory.role != 'builder2')
         //    Game.rooms[creep.room.name].createConstructionSite(creep.pos.x, creep.pos.y, STRUCTURE_ROAD);
-
-        if (!creep.memory.sourceId) {
-            if (Memory.spawns[creep.memory.spawn].store.length) {
-                creep.memory.sourceId = Memory.spawns[creep.memory.spawn].store[0].id;
-                Memory.spawns[creep.memory.spawn].store.splice(0, 1);
-            }
-        }
-
-        if (creep.memory.sourceId) {
-            var creepSource = Game.getObjectById(creep.memory.sourceId);
-
-            if (creepSource) {
-                if (creepSource.transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(creepSource);
+        if (Memory.spawns[creep.memory.spawn].store.length) {
+            if (!creep.memory.sourceId) {
+                if (Memory.spawns[creep.memory.spawn].store.length) {
+                    creep.memory.sourceId = Memory.spawns[creep.memory.spawn].store[0].id;
+                    Memory.spawns[creep.memory.spawn].store.splice(0, 1);
                 }
-
             }
-        }
 
+            if (creep.memory.sourceId) {
+                var creepSource = Game.getObjectById(creep.memory.sourceId);
+
+                if (creepSource) {
+                    if (creepSource.transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(creepSource);
+                    }
+
+                }
+            }
+
+        }
     }
 
 };
