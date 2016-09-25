08a24ded1c71c548bb4a8a91a0a4faf5c890ee20
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-25 10:09:05 +0200

    fix

--- jannes/role.lab.js
+++ jannes/role.lab.js
@@ -2,7 +2,7 @@ var roleLab = {
 
     /** @param {Creep} creep **/
     run: function () {
-        labs[Game.getObjectById('57deceb85b49191922741434')].runReaction(labs[Game.getObjectById('57ddc0f8c4053ddd7aeb3767')], labs[Game.getObjectById('57de0a4844c88be676166f38')]);
+        Game.getObjectById('57deceb85b49191922741434').runReaction(Game.getObjectById('57ddc0f8c4053ddd7aeb3767'), Game.getObjectById('57de0a4844c88be676166f38'));
     }
 };
 
