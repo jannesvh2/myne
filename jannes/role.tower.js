c0627e99a12156f1549422f735fc5549121b1191
jannesvh <jvanhelsdingen@retrorabbitco.za>
2016-09-26 20:32:18 +0200

    fix

--- jannes/role.tower.js
+++ jannes/role.tower.js
@@ -36,7 +36,7 @@ var roleTower = {
                             Memory.spawns[a].random.towerHostiles[hostiles[b].id].delay = 8;
                         }
                     }
-                    Memory.spawns[a].random.towerHostiles[target.name].delay = 15;
+                    Memory.spawns[a].random.towerHostiles[target.id].delay = 15;
                     towers.forEach(tower => tower.attack(target));
                     continue;
                 }
