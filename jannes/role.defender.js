e3e79584aaecb00bb265d16cae584be208a601f8
jannesvh2 <jannesvh@gmail.com>
2016-10-08 22:31:43 +0200

    fixed defenders

--- jannes/role.defender.js
+++ jannes/role.defender.js
@@ -5,14 +5,52 @@ var roleDefenders = {
 
 
         var targets = [];
-        //If not in the correct room, move towards it
+
+        if (creep.memory.sourceRoom == Memory.spawns[creep.memory.spawn].random.mainRoom) {
+            if (!creep.memory.rampartId) {
+                var targets = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+                var rampart = targets.pos.findClosestByRange(FIND_MY_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_RAMPART && !creep.room.lookForAt(LOOK_CREEPS, structure).length)
+                    }
+                });
+                creep.memory.rampartId = rampart.id;
+            }
+            if (!creep.memory.atSpot) {
+                var rampObj = Game.getObjectById(creep.memory.rampartId);
+                if (!rampObj) {
+                    delete creep.memory.rampartId;
+                    return;
+                }
+                if (!creep.pos.isEqualTo(rampObj.pos)) {
+                    creep.moveTo(rampObj);
+                    return;
+                }
+                creep.memory.noTarget = 0;
+                creep.memory.atSpot = true;
+            }
+            var hostiles = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 1);
+            if (hostiles.length)
+                creep.attack(hostiles[0]);
+            else {
+                if (creep.memory.noTarget < 10)
+                    creep.memory.noTarget++;
+                else {
+                    creep.moveTo(Game.spawns['Spawn' + parseInt(creep.memory.spawn) + "" + 0]);
+                    creep.memory.atSpot = false;
+                    delete creep.memory.rampartId;
+                }
+            }
+        }
+        else {
+            //If not in the correct room, move towards it
 
             var targets = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
 
             if (targets) {
 
                 if (creep.attack(targets) == ERR_NOT_IN_RANGE)
-                    if(creep.moveTo(targets) != OK){
+                    if (creep.moveTo(targets) != OK) {
                         if (creep.room.name != creep.memory.sourceRoom) {
                             var exitDir = Game.map.findExit(creep.room.name, creep.memory.sourceRoom);
                             var Exit = creep.pos.findClosestByRange(exitDir);
@@ -27,7 +65,8 @@ var roleDefenders = {
                     creep.moveTo(Exit);
                 }
             }
-        
+        }
+
     }
 };
 
