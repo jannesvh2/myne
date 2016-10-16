7cbd4e4867b4651de903c50e43da04cf0ccee306
jannesvh2 <jannesvh@gmail.com>
2016-10-16 03:39:50 +0200

    fix

--- jannes/role.logging.js
+++ jannes/role.logging.js
@@ -82,7 +82,7 @@ var roleLogging = {
         //+ ", l: " + links.length + "/" + Memory.linkSource.length * 2;
 
         msg6 +=
-            "(Spawn 4) H: " + Memory.spawns[5].creeps.harvesters.length + "/" + Memory.spawns[5].summon.h
+            "(Spawn 5) H: " + Memory.spawns[5].creeps.harvesters.length + "/" + Memory.spawns[5].summon.h
            + ", B: " + Memory.spawns[5].creeps.builders.length + "/" + Memory.spawns[5].summon.b
            + ", U: " + Memory.spawns[5].creeps.upgraders.length + "/" + Memory.spawns[5].summon.u
            + ", d/m/h: " + Memory.spawns[5].creeps.attackersD.length + "/" + Memory.spawns[5].summon.atkD + " " + Memory.spawns[5].creeps.attackersM.length + "/" + Memory.spawns[5].summon.atkM + " " + Memory.spawns[5].creeps.attackersH.length + "/" + Memory.spawns[5].summon.atkH
