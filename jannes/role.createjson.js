8e0716a643c52c0deab59e9bf96f0fc65330ae42
jannesvh <jvanhelsdingen@retrorabbit.co.za>
2016-10-24 10:01:00 +0200

    fix

--- jannes/role.createjson.js
+++ jannes/role.createjson.js
@@ -192,7 +192,7 @@ var roleCreateJSON = {
             //spawn 0
             //spawn numbers
             Memory.spawns[1].summon.spawns = 1;
-            Memory.spawns[1].summon.h = 3;
+            Memory.spawns[1].summon.h = 2;
             Memory.spawns[1].summon.b = 4;
             Memory.spawns[1].summon.u = 2;
             Memory.spawns[1].summon.h2 = 0;
