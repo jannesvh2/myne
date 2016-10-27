868685202cfb66ae8cb0a376bb91f905e85ed5f1
jannesvh2 <jannesvh@gmail.com>
2016-10-27 18:46:24 +0200

    fix

--- jannes/role.repairer.js
+++ jannes/role.repairer.js
@@ -36,11 +36,20 @@ var roleRepairer = {
             }
             if (creep.repair(targetId) == ERR_NOT_IN_RANGE)
                 creep.moveTo(targetId);
-            else
-                if (creep.pos.roomName == Memory.spawns[creep.memory.spawn].random.mainRoom)
-                    Memory.spawns[creep.memory.spawn].repairHpHistory[targetId.id] = targetId.hits;
+            else{
+                if(targetId.hits >=  Memory.spawns[creep.memory.spawn].counters.repairLimit || targetId.hits >=  targetId.hitsMax){
+                    delete creep.memory.type;
+                    delete creep.memory.targetId;
+                    return;
+                }
+            }
+
         }
+        else
+            if (creep.pos.roomName == Memory.spawns[creep.memory.spawn].random.mainRoom)
+                Memory.spawns[creep.memory.spawn].repairHpHistory[targetId.id] = targetId.hits;
     }
+}
 }
 
 module.exports = roleRepairer;
