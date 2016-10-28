b255759a7224b1f1505a6e446039fc6a61d175c6
jannesvh2 <jannesvh@gmail.com>
2016-10-28 19:31:52 +0200

    fix

--- myne/jannes/role.createjson.js
+++ myne/jannes/role.createjson.js
@@ -398,22 +398,22 @@ var roleCreateJSON = {
                     }
                 }
             }
-            //if (!Memory.spawns[a].sources.length)
-            //    for (let myRooms = 0, length = Memory.spawns[a].random.rooms.length; myRooms < length; myRooms++) {
-            //        if (Game.rooms[Memory.spawns[a].random.rooms[myRooms].name]) {
-            //            roomSources = Game.rooms[Memory.spawns[a].random.rooms[myRooms].name].find(FIND_SOURCES);
-            //            for (let b = 0, length2 = roomSources.length; b < length2; b++) {
-            //                Memory.spawns[a].sources.push(roomSources[b]);
-            //            }
-            //            Memory.spawns[a].random.roomContainers = Memory.spawns[a].random.roomContainers.concat(Game.rooms[Memory.spawns[a].random.rooms[myRooms].name].find(FIND_STRUCTURES, {
-            //                filter: (structure) => {
-            //                    return (structure.structureType == STRUCTURE_CONTAINER
-            //                        //&& structure.store.energy > 500
-            //                        );
-            //                }
-            //            }))
-            //        }
-            //    }
+            if (!Memory.spawns[a].sources.length)
+                for (let myRooms = 0, length = Memory.spawns[a].random.rooms.length; myRooms < length; myRooms++) {
+                    if (Game.rooms[Memory.spawns[a].random.rooms[myRooms].name]) {
+                        roomSources = Game.rooms[Memory.spawns[a].random.rooms[myRooms].name].find(FIND_SOURCES);
+                        for (let b = 0, length2 = roomSources.length; b < length2; b++) {
+                            Memory.spawns[a].sources.push(roomSources[b]);
+                        }
+                        Memory.spawns[a].random.roomContainers = Memory.spawns[a].random.roomContainers.concat(Game.rooms[Memory.spawns[a].random.rooms[myRooms].name].find(FIND_STRUCTURES, {
+                            filter: (structure) => {
+                                return (structure.structureType == STRUCTURE_CONTAINER
+                                    //&& structure.store.energy > 500
+                                    );
+                            }
+                        }))
+                    }
+                }
 
             if (Memory.spawns[a].counters.avgUpgraders > 1000) {
                 Memory.spawns[a].counters.avgUpgraders = 0;
