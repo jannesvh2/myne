39c1d10b9d29556338cef71d29031bffffe0d5e4
jannesvh2 <jannesvh@gmail.com>
2016-10-28 21:59:46 +0200

    fix

--- myne/jannes/role.createjson.js
+++ myne/jannes/role.createjson.js
@@ -313,9 +313,9 @@ var roleCreateJSON = {
             //spawn 3
             //spawn numbers
             Memory.spawns[3].summon.spawns = 1;
-            Memory.spawns[3].summon.h = 2;
+            Memory.spawns[3].summon.h = 3;
             Memory.spawns[3].summon.b = 1;
-            Memory.spawns[3].summon.u = 5;
+            Memory.spawns[3].summon.u = 6;
             Memory.spawns[3].summon.h2 = 0;
             Memory.spawns[3].summon.b2 = 0;
             Memory.spawns[3].summon.u2 = 0;
