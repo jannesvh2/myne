3c65e65db28557328569072d09d2ab34fda1c088
jannesvh2 <jannesvh@gmail.com>
2016-10-28 20:14:05 +0200

    fixed keeper

--- myne/jannes/role.keeper.js
+++ myne/jannes/role.keeper.js
@@ -7,28 +7,14 @@ var roleKeeper = {
         if (hostiles && creep.room.name != Memory.spawns[creep.memory.spawn].random.mainRoom)
             Memory.spawns[creep.memory.spawn].random.defenders.push(creep.room.name);
 
-        for (let spot = 0, length = Memory.spawns[creep.memory.spawn].spots.length; spot < length; spot++) {
-            //If not in the correct room, move towards it
-            if (creep.memory.sourceRoom == Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom) {
-                if (Game.rooms[creep.memory.sourceRoom]) {
-                    let reserveCheck = creep.reserveController(Game.rooms[creep.memory.sourceRoom].controller);
-                    if (reserveCheck == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(new RoomPosition(Game.rooms[creep.memory.sourceRoom].controller.pos.x, Game.rooms[creep.memory.sourceRoom].controller.pos.y, creep.memory.sourceRoom));
-
-                    }
-                }
-                else {
-                    if (creep.room.name != Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom && Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom != '') {
-                        var exitDir = Game.map.findExit(creep.room.name, Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom);
-                        var Exit = creep.pos.findClosestByRange(exitDir);
-                        creep.moveTo(Exit);
-                    }
-                    else if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(creep.room.controller);
-
-                    }
-                }
-            }
+        if (creep.memory.sourceRoom == creep.room.name) {
+            if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE)
+                creep.moveTo(creep.room.controller);
+        }
+        else {
+            var exitDir = Game.map.findExit(creep.room.name, Memory.spawns[creep.memory.spawn].spots[spot].sourceRoom);
+            var Exit = creep.pos.findClosestByRange(exitDir);
+            creep.moveTo(Exit);
         }
 
 
