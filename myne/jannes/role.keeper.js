5a036ceee69e3d73dade51f12481726f27867319
jannesvh2 <jannesvh@gmail.com>
2016-10-28 20:25:28 +0200

    fix

--- myne/jannes/role.keeper.js
+++ myne/jannes/role.keeper.js
@@ -7,9 +7,10 @@ var roleKeeper = {
         if (hostiles && creep.room.name != Memory.spawns[creep.memory.spawn].random.mainRoom)
             Memory.spawns[creep.memory.spawn].random.defenders.push(creep.room.name);
 
-        if (creep.memory.sourceRoom == creep.room.name) {
-            if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE)
-                creep.moveTo(creep.room.controller);
+        let controller = Game.rooms[creep.memory.sourceRoom].controller;
+        if (controller) {
+            if (creep.reserveController(controller) == ERR_NOT_IN_RANGE)
+                creep.moveTo(controller);
         }
         else {
             var exitDir = Game.map.findExit(creep.room.name, creep.memory.sourceRoom);
