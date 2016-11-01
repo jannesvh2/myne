cea517a8c0b729e45098e86155f990b926f42c0e
jannesvh2 <jannesvh@gmail.com>
2016-11-01 17:31:25 +0200

    fix

--- myne/jannes/role.user.js
+++ myne/jannes/role.user.js
@@ -48,7 +48,7 @@ var roleUser = {
                     creep.moveTo(targets);
                 }
             }
-            else if (Memory.spawns[creep.memory.spawn].random.terminal && Game.getObjectById(Memory.spawns[creep.memory.spawn].random.terminal).store.energy < 30000) {
+            else if (Memory.spawns[creep.memory.spawn].random.terminal && Game.getObjectById(Memory.spawns[creep.memory.spawn].random.terminal).store.energy < 50000) {
                 var terminal = Game.getObjectById(Memory.spawns[creep.memory.spawn].random.terminal);
                 if (creep.transfer(terminal, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
                     creep.moveTo(terminal);
