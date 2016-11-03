7fe2e5bdec2465734e8f206ddabddf51b2eda24d
jannesvh2 <jannesvh@gmail.com>
2016-11-03 21:55:38 +0200

    fix

--- myne/jannes/role.createjson.js
+++ myne/jannes/role.createjson.js
@@ -432,7 +432,7 @@ var roleCreateJSON = {
             Memory.spawns[5].summon.h = 0;
             Memory.spawns[5].summon.b = 0;
             Memory.spawns[5].summon.u = 0;
-            Memory.spawns[5].summon.h2 = 4;
+            Memory.spawns[5].summon.h2 = 5;
             Memory.spawns[5].summon.b2 = 1;
             Memory.spawns[5].summon.u2 = 0;
             Memory.spawns[5].summon.users = 1;
@@ -449,7 +449,7 @@ var roleCreateJSON = {
             Memory.spawns[5].random.upgradeSpots.push({ x: 35, y: 34 });
 
             ////RoomList
-            Memory.spawns[5].random.rooms.push({ name: 'W6S51', spawn: 1 });
+            Memory.spawns[5].random.rooms.push({ name: 'W6S51', spawn: 2 });
             if (!Memory.spawns[5].random.hostiles) {
                 Memory.spawns[5].random.rooms.push({ name: 'W5S51', spawn: 3 });
             //    //Memory.spawns[5].random.rooms.push({ name: 'W7S56', spawn: 3 });
