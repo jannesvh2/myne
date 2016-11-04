ca89a9d6953d1dd0cd8bbfa2cb8174901976756d
jannesvh <jvanhelsdingen@retrorabbit.co.za>
2016-11-04 11:54:05 +0200

    fix

--- myne/jannes/role.spawn.js
+++ myne/jannes/role.spawn.js
@@ -361,8 +361,8 @@ var roleSpawn = {
                 Memory.spawns[spawn].random.hostiles = false;
                 return;
             }
-            if (spawn == 0 || spawn == 1) {
-                if (Memory.spawns[6].creeps.helpers.length < 4) {
+            if (spawn == 0) {
+                if (Memory.spawns[6].creeps.helpers.length < 0) {
                     if (Game.rooms[Memory.spawns[spawn].random.mainRoom].energyCapacityAvailable < 5000)
                         newName = multiSpawn([WORK, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, WORK, WORK, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, WORK, WORK, WORK, WORK, MOVE, WORK, WORK, MOVE, MOVE], { role: 'builder', spawn: 6, helper: true });
                     else
