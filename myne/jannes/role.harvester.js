f9a8195de282f5624cc93f4b24cfa026050406b4
jannesvh2 <jannesvh@gmail.com>
2016-11-08 17:37:20 +0200

    fix

--- myne/jannes/role.harvester.js
+++ myne/jannes/role.harvester.js
@@ -137,9 +137,10 @@ var roleHarvester = {
                     if (targets) {
                         var path = creep.room.findPath(creep.pos, targets.pos);
                         console.log(path.length);
-                        if (path.length < 7)
+                        if (path.length < 7) {
                             creep.memory.targetId = targets.id;
-                        creep.move(path[0].direction);
+                            creep.move(path[0].direction);
+                        }
                     }
 
                 }
