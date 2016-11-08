6908b4ae6858370133617d185ff13749d5817e92
jannesvh <jvanhelsdingen@retrorabbit.co.za>
2016-11-08 14:48:22 +0200

    fix

--- myne/jannes/role.link.js
+++ myne/jannes/role.link.js
@@ -3,14 +3,23 @@ var roleLink = {
     /** @param {Creep} creep **/
     run: function (spawn) {
         for (let a = 0, length = Memory.spawns[spawn].links.producers.length; a < length; a++) {
+            var linkFrom = Game.getObjectById(Memory.spawns[spawn].links.producers[a].id);
+            if (linkFrom.energy < 200)
+                continue;
+            if (Memory.spawns[spawn].links.receiverc) {
+                var linkToC = Game.getObjectById(Memory.spawns[spawn].links.receiverc);
+                if (linkToC && linkToC.energy < 600 && Memory.spawns[spawn].links.producers[a].source) {
+                    linkFrom.transferEnergy(linkToC);
+                    continue;
+                }
+                if (linkToC && linkToC.energy < 200)
+                    linkTo.transferEnergy(linkToC);
+            }
 
-            var linkFrom = Game.getObjectById(Memory.spawns[spawn].links.producers[a]);
             var linkTo = Game.getObjectById(Memory.spawns[spawn].links.receiver);
             if (!linkFrom || !linkTo)
                 continue;
 
-            if (linkFrom.energy < 200)
-                continue;
 
             linkFrom.transferEnergy(linkTo);
         }
