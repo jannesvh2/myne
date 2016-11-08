7455df607418fe9c34804cdf3e6efa01ab2f78ca
jannesvh <jvanhelsdingen@retrorabbit.co.za>
2016-11-08 11:12:56 +0200

    fix

--- myne/jannes/role.createjson.js
+++ myne/jannes/role.createjson.js
@@ -145,7 +145,7 @@ var roleCreateJSON = {
             Memory.spawns[0].summon.h = 0;
             Memory.spawns[0].summon.b = 0;
             Memory.spawns[0].summon.u = 0;
-            Memory.spawns[0].summon.h2 = 6;
+            Memory.spawns[0].summon.h2 = 7;
             Memory.spawns[0].summon.b2 = 1;
             Memory.spawns[0].summon.u2 = 0;
             Memory.spawns[0].summon.users = 1;
@@ -163,7 +163,7 @@ var roleCreateJSON = {
             //RoomList
             Memory.spawns[0].random.rooms.push({ name: 'W4S59', spawn: 0 });
             if (!Memory.spawns[0].random.hostiles) {
-                Memory.spawns[0].random.rooms.push({ name: 'W3S59', spawn: 1 });
+                Memory.spawns[0].random.rooms.push({ name: 'W3S59', spawn: 2 });
                 Memory.spawns[0].random.rooms.push({ name: 'W5S59', spawn: 1 });
                 Memory.spawns[0].random.rooms.push({ name: 'W6S59', spawn: 4 });
             }
