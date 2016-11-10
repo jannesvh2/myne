f6623e0c73e8ad8ebd8385a920736ef95216ca1b
jannesvh <jvanhelsdingen@retrorabbit.co.za>
2016-11-10 13:55:23 +0200

    fix

--- myne/jannes/role.prototypes.js
+++ myne/jannes/role.prototypes.js
@@ -2,19 +2,15 @@ var rolePrototypes = {
 
     /** @param {Creep} creep **/
     run: function () {
-        Object.defineProperty(Room.prototype, 'destroy', {
-            get: function () {
-                let destroy = this.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_WALL)
-                    }
-                });
-                for (var d of destroy)
-                    d.destroy();
-            },
-            enumerable: false,
-            configurable: true
-        });
+        Room.prototype.destroy = function () {
+            let destroy = this.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_WALL)
+                }
+            });
+            for (var d of destroy)
+                d.destroy();
+        };
     }
 };
 
