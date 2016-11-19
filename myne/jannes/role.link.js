e3c5b97fc7b78ec534b8551d3eb91e10d94b9433
jannesvh <jvanhelsdingen@retrorabbit.co.za>
2016-11-19 12:56:37 +0200

    fix

--- myne/jannes/role.link.js
+++ myne/jannes/role.link.js
@@ -8,22 +8,24 @@ var roleLink = {
         if (!linkTo)
             return;
 
-        if(Memory.spawns[spawn].links.receiverC)
+        if (Memory.spawns[spawn].links.receiverC)
             linkToC = Game.getObjectById(Memory.spawns[spawn].links.receiverC);
 
         for (let a = 0, length = Memory.spawns[spawn].links.producers.length; a < length; a++) {
             var linkFrom = Game.getObjectById(Memory.spawns[spawn].links.producers[a].id);
-            if(!linkFrom)
+
+            if (!linkFrom)
                 continue;
             if (linkFrom.energy < 200)
                 continue;
 
             if (linkToC && Memory.spawns[spawn].links.producers[a].source && linkToC.energy < 600) {
-                    linkFrom.transferEnergy(linkToC);
-                    continue;
+                linkFrom.transferEnergy(linkToC);
+                continue;
             }
 
-            linkFrom.transferEnergy(linkTo);
+            if (linkTo.energy < 300)
+                linkFrom.transferEnergy(linkTo);
         }
 
 
