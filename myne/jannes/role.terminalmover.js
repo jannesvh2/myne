8c952def93bd7a4e0d545f07c1a07257e85a483a
jannesvh <jvanhelsdingen@retrorabbit.co.za>
2016-11-21 11:09:05 +0200

    fix

--- myne/jannes/role.terminalmover.js
+++ myne/jannes/role.terminalmover.js
@@ -3,31 +3,65 @@ var roleTerminalMover = {
     /** @param {Creep} creep **/
     run: function (creep) {
         //Game.spawns['Spawn00'].createCreep([CARRY, CARRY, MOVE], null, { role: 'terminal', spawn: 0 });
-        for (let a = 0, length = Memory.spawns.length; a < length; a++) {
-            if (Memory.spawns[a].random.runReaction) {
-                var total = _.sum(creep.carry);
-                if (creep.memory.full && total == 0) {
-                    creep.memory.full = false;
-                }
-                else if (!creep.memory.full && total == creep.carryCapacity) {
-                    creep.memory.full = true;
-                }
 
-                let terminal = Game.getObjectById('57dc6e2fe2d7cc5a33ae4da2');
-                if (creep.memory.full) {
-                    let lab = Game.getObjectById('57ddc0f8c4053ddd7aeb3767');
 
-                    if (creep.transfer(lab, "X") == ERR_NOT_IN_RANGE)
-                        creep.moveTo(lab);
-                }
-                else {
+        let terminal = Game.getObjectById(Memory.spawns[creep.memory.spawn].random.terminal);
+        if (!terminal)
+            return;
+        let moveTo = Game.getObjectById(creep.memory.work.moveTo);
+        if (creep.memory.work.isLab) {
+            if (moveTo.mineralAmount >= creep.memory.work.fillTo)
+                delete creep.memory.work;
+        }
+        else {
+            if (moveTo.terminal.store[creep.memory.work.moveType] >= creep.memory.work.fillTo)
+                delete creep.memory.work;
 
-                    if (creep.withdraw(terminal, "X") == ERR_NOT_IN_RANGE)
-                        creep.moveTo(terminal);
+        }
+        if (!creep.memory.work) {
+            if (terminal.store['XUH2O']) {
+                let lab = Game.getObjectById(Memory.spawns[creep.memory.spawn].random.defLab);
+                if (lab && lab.mineralType == 'XUH2O' && lab.mineralAmount < 1000) {
+                    creep.memory.work = {};
+                    creep.memory.work.moveType == 'XUH2O';
+                    creep.memory.work.moveTo = lab;
+                    creep.memory.work.moveFrom = terminal;
+                    creep.memory.work.fillTo = 3000;
+                    creep.memor.worky.isLab = true;
                 }
+            }
+
+            if (!creep.memory.work) {
+                //reactions go here
             }
         }
 
+        var total = _.sum(creep.carry);
+        if (total.length && (total.length > 1 || !total[0][creep.memory.moveType])) {
+            for (var carry in total) {
+                if (creep.transfer(terminal, carry) == ERR_NOT_IN_RANGE)
+                    creep.moveTo(terminal);
+                return;
+            }
+        }
+
+        if (creep.memory.full && total == 0) {
+            creep.memory.full = false;
+        }
+        else if (!creep.memory.full && total == creep.carryCapacity) {
+            creep.memory.full = true;
+        }
+
+        if (creep.memory.full) {
+
+            if (creep.transfer(moveTo, creep.memory.moveType) == ERR_NOT_IN_RANGE)
+                creep.moveTo(moveTo);
+        }
+        else {
+
+            if (creep.withdraw(moveFrom, creep.memory.moveType) == ERR_NOT_IN_RANGE)
+                creep.moveTo(moveFrom);
+        }
     }
 };
 
