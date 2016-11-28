cdb30c404cd0963bdae9e07b9b7e052b31e1efd2
jannesvh <jvanhelsdingen@retrorabbit.co.za>
2016-11-28 14:58:00 +0200

    fix

--- myne/jannes/role.store.js
+++ myne/jannes/role.store.js
@@ -76,7 +76,7 @@ var roleStore = {
                 }
                 let skCheck = Game.getObjectById(creep.memory.skSpawn);
 
-                if (skCheck && skCheck.ticksToSpawn < 5) {
+                if (skCheck && skCheck.ticksToSpawn < 6) {
                     creep.memory.run = true;
                 }
             }
