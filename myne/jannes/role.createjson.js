5c954d8c1944e2f4d0817bf3ab0e8178012b2f48
jannesvh2 <jannesvh@gmail.com>
2016-11-30 19:45:43 +0200

    fix

--- myne/jannes/role.createjson.js
+++ myne/jannes/role.createjson.js
@@ -196,8 +196,7 @@ var roleCreateJSON = {
             Memory.spawns[0].random.extractor = '579fab88b1f02a3b0cff0343';
             Memory.spawns[0].random.terminal = '581790d7ef165c5f66b0de2b';
             Memory.spawns[0].random.defLab = '5834cc21443e2b9d39429d48';
-
-            Memory.spawns[0].random.overflow = 'W6S53';
+            Memory.spawns[0].random.overflow = 'W3S55';
             //Memory.spawns[0].random.nuker = '5834f6c016bc32e01a5f3996';
 
             //reactions
@@ -215,7 +214,7 @@ var roleCreateJSON = {
 
             //spawn 1
             //spawn numbers
-            Memory.spawns[1].summon.spawns = 2;
+            Memory.spawns[1].summon.spawns = 3;
             Memory.spawns[1].summon.h = 0;
             Memory.spawns[1].summon.b = 0;
             Memory.spawns[1].summon.u = 0;
@@ -258,6 +257,7 @@ var roleCreateJSON = {
             Memory.spawns[1].random.extractor = '579fab87b1f02a3b0cff02fb';
             Memory.spawns[1].random.terminal = '581a76d942f613dc4a3544a6';
             Memory.spawns[1].random.defLab = '';
+            Memory.spawns[1].random.overflow = 'W3S55';
 
             //spawn 2
             //spawn numbers
@@ -309,7 +309,7 @@ var roleCreateJSON = {
             Memory.spawns[2].random.extractor = '579fab87b1f02a3b0cff02f4';
             Memory.spawns[2].random.terminal = '581d19ac83ddc28b14a43a4e';
             Memory.spawns[2].random.defLab = '58389bd48ff1bfa430ad5d91';
-            Memory.spawns[2].random.overflow = 'W6S53';
+            Memory.spawns[2].random.overflow = 'W3S55';
 
             //spawn 3
             //spawn numbers
@@ -408,6 +408,7 @@ var roleCreateJSON = {
             Memory.spawns[4].random.extractor = '579fab87b1f02a3b0cff030a';
             Memory.spawns[4].random.terminal = '581e93ab1ba2e0cc73e157bf';
             Memory.spawns[4].random.defLab = '';
+            Memory.spawns[4].random.overflow = 'W3S55';
 
             //spawn 5
             //spawn numbers
