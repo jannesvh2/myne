1ca92d9133357aeffa7be1b9b6762cf0a99343e1
jannesvh2 <jannesvh@gmail.com>
2016-12-01 22:29:57 +0200

    fix

--- myne/jannes/role.spawn.js
+++ myne/jannes/role.spawn.js
@@ -357,7 +357,7 @@ var roleSpawn = {
             }
             let lab = Game.getObjectById(Memory.spawns[spawn].random.defLab);
             if (!Memory.spawns[spawn].creeps.terminals.length && (Memory.spawns[spawn].random.runReaction || (terminal && terminal.store['XUH2O'] && lab && lab.mineralType == 'XUH2O' && lab.mineralAmount < 1000))) {
-                newName = multiSpawn([MOVE, CARRY, CARRY, CARRY, CARRY, MOVE], { role: 'terminal', spawn: spawn });
+                newName = multiSpawn([MOVE, CARRY, CARRY], { role: 'terminal', spawn: spawn });
                 return;
             }
 
