ff8d50d06bb75b84d0e4ecf5c12ef107d371c233
jannesvh2 <jannesvh@gmail.com>
2016-12-01 21:17:22 +0200

    fix

--- myne/jannes/role.lab.js
+++ myne/jannes/role.lab.js
@@ -4,8 +4,8 @@ var roleLab = {
     run: function () {
         for (let a = 0, length = Memory.spawns.length; a < length; a++) {
             if (Memory.spawns[a].random.runReaction) {
-                for (let s = 0, lengthS = Memory.spawns[creep.memory.spawn].reactions.length; s < lengthS; s++) {
-                    Game.getObjectById(Memory.spawns[creep.memory.spawn].reactions[s][0]).runReaction(Game.getObjectById(Memory.spawns[creep.memory.spawn].reactions[s][1]), Game.getObjectById(Memory.spawns[creep.memory.spawn].reactions[s][2]));
+                for (let s = 0, lengthS = Memory.spawns[a].reactions.length; s < lengthS; s++) {
+                    Game.getObjectById(Memory.spawns[a].reactions[s][0]).runReaction(Game.getObjectById(Memory.spawns[a].reactions[s][1]), Game.getObjectById(Memory.spawns[a].reactions[s][2]));
                 }
             }
         }
