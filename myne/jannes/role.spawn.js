4e042afa31e2d3f20329681d4ce67db98bf01fee
jannesvh2 <jannesvh@gmail.com>
2016-12-17 20:20:59 +0200

    fix

--- myne/jannes/role.spawn.js
+++ myne/jannes/role.spawn.js
@@ -368,7 +368,7 @@ var roleSpawn = {
                 let boosting = Memory.spawns[spawn].random.boostNumber;
                 let store = Game.getObjectById(Memory.spawns[boosting].random.storeId);
                 if (store) {
-                    storeVal = (store.energy - 400000) / 100000 + 1 | 0;
+                    storeVal = (store.store.energy - 400000) / 100000 + 1 | 0;
                     if (Memory.spawns[boosting].creeps.upgraders2.length < (storeVal < 2 ? 2 : storeVal)) {
                         let getBoost = false;
                         if (Memory.spawns[boosting].random.upgradeBoost)
