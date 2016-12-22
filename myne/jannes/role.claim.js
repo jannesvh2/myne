04189268b95c1eea5b104d085ace2d79b89a19fc
jannesvh2 <jannesvh@gmail.com>
2016-12-22 14:09:24 +0200

    fix

--- myne/jannes/role.claim.js
+++ myne/jannes/role.claim.js
@@ -4,39 +4,39 @@ var roleClaim = {
     run: function (creep) {
         //Game.spawns['Spawn20'].createCreep([MOVE, CLAIM], undefined, { role: 'claim', spawn: 2, sourceRoom: 'W52S28'});
         if (creep.room.name != creep.memory.sourceRoom) {
-            var flag = Game.flags['Flag7'];
-            if (flag) {
-                if (!creep.memory.flag || creep.memory.flag == 1) {
-                    creep.moveTo50(flag);
-                    if (flag.room.name == creep.room.name)
-                        creep.memory.flag = 2;
-                    return;
-                }
-                if (creep.memory.flag == 2) {
+            //var flag = Game.flags['Flag7'];
+            //if (flag) {
+            //    if (!creep.memory.flag || creep.memory.flag == 1) {
+            //        creep.moveTo50(flag);
+            //        if (flag.room.name == creep.room.name)
+            //            creep.memory.flag = 2;
+            //        return;
+            //    }
+            //    if (creep.memory.flag == 2) {
 
-                    flag = Game.flags['Flag3'];
-                    creep.moveTo50(flag);
-                    if (flag.room.name == creep.room.name)
-                        creep.memory.flag = 3;
-                    return;
-                }
-                if (creep.memory.flag == 3) {
+            //        flag = Game.flags['Flag3'];
+            //        creep.moveTo50(flag);
+            //        if (flag.room.name == creep.room.name)
+            //            creep.memory.flag = 3;
+            //        return;
+            //    }
+            //    if (creep.memory.flag == 3) {
 
-                    flag = Game.flags['Flag4'];
-                    creep.moveTo50(flag);
-                    if (flag.room.name == creep.room.name)
-                        creep.memory.flag = 4;
-                    return;
-                }
-                if (creep.memory.flag == 4) {
+            //        flag = Game.flags['Flag4'];
+            //        creep.moveTo50(flag);
+            //        if (flag.room.name == creep.room.name)
+            //            creep.memory.flag = 4;
+            //        return;
+            //    }
+            //    if (creep.memory.flag == 4) {
 
-                    flag = Game.flags['Flag4'];
-                    creep.moveTo50(flag);
-                    return;
-                }
+            //        flag = Game.flags['Flag4'];
+            //        creep.moveTo50(flag);
+            //        return;
+            //    }
 
 
-            }
+            //}
             var exitDir = Game.map.findExit(creep.room.name, creep.memory.sourceRoom);
             var Exit = creep.pos.findClosestByRange(exitDir);
             creep.moveTo50(Exit, { canOn: true });
