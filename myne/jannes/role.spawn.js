ddf9e8c5633a8ed8f14adfff6be3321ac8f4a5b5
jannesvh2 <jannesvh@gmail.com>
2017-01-05 22:07:12 +0200

    fix

--- myne/jannes/role.spawn.js
+++ myne/jannes/role.spawn.js
@@ -411,7 +411,7 @@ var roleSpawn = {
             }
 
             if (spawn == 0) {
-                if (Memory.spawns[11].creeps.helpers.length < 3) {
+                if (Memory.spawns[11].creeps.helpers.length < 4) {
                     if (Game.rooms[Memory.spawns[spawn].random.mainRoom].energyCapacityAvailable < 5000)
                         newName = multiSpawn([WORK, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, WORK, WORK, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, WORK, WORK, WORK, WORK, MOVE, WORK, WORK, MOVE, MOVE], { role: 'builder', spawn: 6, helper: true });
                     else
