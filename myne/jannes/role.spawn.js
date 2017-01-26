b4fba3d5f95da0fb7478b93df9806cf863ceec75
jannesvh <jvanhelsdingen@retrorabbit.co.za>
2017-01-26 11:49:08 +0200

    fix

--- myne/jannes/role.spawn.js
+++ myne/jannes/role.spawn.js
@@ -73,7 +73,7 @@ var roleSpawn = {
                 return;
             }
             //Game.spawns['Spawn00'].createCreep([CARRY, CARRY, MOVE], null, { role: 'terminal', spawn: 0 });
-            if (Memory.spawns[spawn].random.useLinks && !Memory.spawns[spawn].creeps.movers.length || Memory.spawns[spawn].creeps.movers.length < 2 && Memory.spawns[spawn].creeps.movers[0] && Game.creeps[Memory.spawns[spawn].creeps.movers[0]].ticksToLive < 75) {
+            if ((Memory.spawns[spawn].random.useLinks && !Memory.spawns[spawn].creeps.movers.length) || (Memory.spawns[spawn].creeps.movers.length < 2 && Memory.spawns[spawn].creeps.movers[0] && Game.creeps[Memory.spawns[spawn].creeps.movers[0]].ticksToLive < 75)) {
                 newName = multiSpawn([CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE], { role: 'mover', spawn: spawn });
                 return;
             }
